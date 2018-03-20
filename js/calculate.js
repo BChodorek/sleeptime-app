62047aa0f3b407b1fbf5c702d79d6c3492d4b72c
Bartłomiej <bartlomiej.chodorek@gmail.com>
2018-03-20 22:12:18 +0100

    JS fix

--- js/calculate.js
+++ js/calculate.js
@@ -6,11 +6,17 @@ let zzz = document.getElementById("now");
 
 let hoursNow = new Date().getHours();
 let minutesNow = new Date().getMinutes();
+
 let hourTab = [];
 let minutesTab = [];
 
 //Kalkulacja cyklów na podstawie obecnego czasu
 function cycles() {
+
+  hoursNow = new Date().getHours();
+  minutesNow = new Date().getMinutes();
+  output.innerHTML = null;
+
   for (let i = 1; i < 7; i++) {
     let cycleHour = 0;
     let cycleMinute = 0;
@@ -19,7 +25,7 @@ function cycles() {
     cycleMinute += ((minutesNow + (i * 30) + 15) % 60);
     hourTab.push(cycleHour);
     minutesTab.push(cycleMinute);
-    output.innerHTML += 'Godzina pobudki:  ' + hourTab[i - 1] + ':' + minutesTab[i - 1] + ". " + ' ' + ' Ilość cykli: ' + [i] + '<br>';
+    output.innerHTML += 'Godzina pobudki:  ' + hourTab[i - 1] + ':' + minutesTab[i - 1] + '' + ". " + ' ' + ' Ilość cykli: ' + [i] + '<br>';
   }
 };
 

e0d11376989490d69290fd2d356b8ac94b572090
Bartłomiej <bartlomiej.chodorek@gmail.com>
2018-03-20 21:17:22 +0100

    Alpha v1

---
+++ js/calculate.js
@@ -0,0 +1,31 @@
+let pickHour = document.getElementById("hour").value;
+let pickMin = document.getElementById("minute").value;
+let pickAP = document.getElementById("ampm").value;
+let output = document.querySelector("body");
+let zzz = document.getElementById("now");
+
+let hoursNow = new Date().getHours();
+let minutesNow = new Date().getMinutes();
+let hourTab = [];
+let minutesTab = [];
+
+//Kalkulacja cyklów na podstawie obecnego czasu
+function cycles() {
+  output.innerHTML = null;
+  for (let i = 1; i < 7; i++) {
+    let cycleHour = 0;
+    let cycleMinute = 0;
+
+    cycleHour += ((hoursNow + i) % 24);
+    cycleMinute += ((minutesNow + (i * 30) + 15) % 60);
+    hourTab.push(cycleHour);
+    minutesTab.push(cycleMinute);
+    output.innerHTML += 'Godzina pobudki:  ' + hourTab[i - 1] + ':' + minutesTab[i - 1] + ". " + ' ' + ' Ilość cykli: ' + [i] + '<br>';
+  }
+};
+
+zzz.addEventListener('click', cycles);
+
+
+//Funkcja kalkulująca cykle snu na podstawie danych podanych w tabelce Hour Minut AM//PM
+//function calculate() {}

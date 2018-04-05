9a252d12aee56768254f73ba970db84b8a696384
Bartłomiej <bartlomiej.chodorek@gmail.com>
2018-04-05 21:56:54 +0200

    Major styling and minor improvements

--- js/calculate.js
+++ js/calculate.js
@@ -1,4 +1,5 @@
 let output = document.getElementById("output-text");
+//let outputContent = document.createElement('p');
 let buttonCount = document.getElementById("calculate-button");
 let pickHour = document.getElementById("hour");
 let pickMin = document.getElementById("minute");
@@ -39,14 +40,16 @@ function formatFix() {
 //Counting the best time to wake up between the cycles if you want to fall asleep now
 function countCycles() {
   output.innerHTML = null;
+  output.innerHTML = 'Try to wake up at following hours in order to wake up rested: ' + "<br>" + "<br>";
   now = new Date();
   for (let i = 0; i < 6; i++) {
     now = new Date(now.getTime() + interval);
     rounding();
     mins += 14; //It takes 14 minutes in average to fall asleep
     formatFix();
-    output.innerHTML += 'Number of cycles: ' + [i + 1] + '<br>' + 'Wake up hour:  ' + hours + ":" + mins + '<br>' + "<br>";
+    output.innerHTML += '| ' + hours + ":" + mins + ' ';
   }
+  output.innerHTML += '|' + '<br>' + '<br>';
 };
 zzz.addEventListener('click', countCycles);
 
@@ -59,19 +62,21 @@ function calculate() {
     alert("ERROR. Pick the correct values.")
   } else {
     let time = pickHour * 3600000 + pickMin * 60000; //+ 54000000; //! explanation at the bottom
-    output.innerHTML += "It would be best if you fall asleep at one the of following hours: " + "<br>";
+    output.innerHTML += "It would be best if you fall asleep at one the of following hours: " + "<br>" + "<br>";
     now = new Date().setTime(0) + time + 54000000; //setting time to 0 milliseconds
     //    now += time; //setting current time to be just the Hour and Minute user chose
     for (let i = 0; i < 4; i++) {
       roundingSet();
       formatFix();
-      output.innerHTML += hours + ":" + mins + "<br>";
+      output.innerHTML += "| " + hours + ":" + mins + ' ';
       now += interval;
     }
+    output.innerHTML += '|' + '<br>' + '<br>';
+
   }
 };
 buttonCount.addEventListener('click', calculate);
 
-/* !In a variable 'time'(line 74) I convert selected hour and minute to milliseconds 
+/* !In a variable 'time' I convert selected hour and minute to milliseconds 
     and I also add 15 hours, because here program have to calculate last 4 cycles of sleep.
     So it's easier to add 15 hours and then count the intervals instead of going backwards with the intervals. */
